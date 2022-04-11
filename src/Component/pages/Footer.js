3564cb6cb33ed0df8d8fecac84ab2db1b9bddc72
sajib581 <sajibdas581@gmail.com>
2022-04-11 15:07:38 +0600

    client done

--- src/Component/pages/Footer.js
+++ src/Component/pages/Footer.js
@@ -1,11 +1,71 @@
-import React from 'react';
+import {
+    faFacebook,
+    faGithub,
+    faInstagram
+} from "@fortawesome/free-brands-svg-icons";
+import { faEarth } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import "../../styles/Footer.css";
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <footer className="mt-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://i.ibb.co/6BkH9FZ/Logo.png" alt="" />
+            <p className="mt-4 ">Living Ecosystem Decentralized Token </p>
+            <div className="d-flex">
+              <FontAwesomeIcon className="mr-3 icon" icon={faGithub} />
+              <FontAwesomeIcon className="mr-3 icon" icon={faFacebook} />
+              <FontAwesomeIcon className="mr-3 icon" icon={faInstagram} />
+              <FontAwesomeIcon className="mr-3 icon" icon={faEarth} />
+            </div>
+          </div>
+          <div className="col-md-2">
+            <h6 className="font-weight-bold">Pages </h6>
+            <ul className="list-unstyled mt-4">
+              <li className="text-secondary">Network Statistic </li>
+              <li className="text-secondary">What is AkaCoin </li>
+              <li className="text-secondary">Wallets </li>
+              <li className="text-secondary">Quick Start Guide </li>
+            </ul>
+          </div>
+          <div className="col-md-2">
+            <h6 className="font-weight-bold">Learn </h6>
+            <ul className="list-unstyled mt-4">
+              <li className="text-secondary">Blog </li>
+              <li className="text-secondary">Video </li>
+              <li className="text-secondary">Podcast </li>
+              <li className="text-secondary">Network states </li>
+            </ul>
+          </div>
+          <div className="col-md-2">
+            <h6 className="font-weight-bold">Support </h6>
+            <ul className="list-unstyled mt-4">
+              <li className="text-secondary">Custommer Service </li>
+              <li className="text-secondary">FAQ </li>
+              <li className="text-secondary">Community </li>
+            </ul>
+          </div>
+          <div className="col-md-2">
+            <h6 className="font-weight-bold">About AKA.Coin </h6>
+            <ul className="list-unstyled mt-4">
+              <li className="text-secondary">About us </li>
+              <li className="text-secondary">Privacy policy </li>
+              <li className="text-secondary">Term of use </li>
+              <li className="text-secondary">Quick start guide</li>
+              <li className="text-secondary">Language support </li>
+              <li className="text-secondary">Cookie policy</li>
+            </ul>
+          </div>
+        </div>   
+        <p className="text-center">©2022 AKA.COIN. All right reserved</p>     
+      </div>
+      
+    </footer>
+  );
 };
 
 export default Footer;
--- src/Component/pages/NetworkStatistics.js
+++ src/Component/pages/NetworkStatistics.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import '../../styles/Network_Statistics.css';
 
 const NetworkStatistics = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="network">
+      <div className="container px-5">
+        <div className="row text-white py-4">
+          <div className="col-3">
+            <p>Transactions/Second</p>
+            <h3>2,905</h3>
+          </div>
+
+          <div className="col-5">
+            <p> Total Transactions</p>
+            <h3>$64,540,470,129</h3>
+          </div>
+
+          <div className="col-2">
+            <p>Avg. Cost/Transaction </p>
+            <h3>$0.00025</h3>
+          </div>
+
+          <div className="col-2">
+            <p>Validator Nodes</p>
+            <h3>1,645</h3>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default NetworkStatistics;
